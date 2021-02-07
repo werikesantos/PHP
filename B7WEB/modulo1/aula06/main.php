bbc79d41b3097d53b03381fe4f3a0490761219f2
werikesantos <werike.ms@gmail.com>
2021-02-07 00:22:10 -0300

    Imprimindo uma mensagem na tela

---
+++ B7WEB/modulo1/aula06/main.php
@@ -0,0 +1,16 @@
+<?php
+
+    include_once '../../../B7WEB/menu.html';
+    include_once '../../../B7WEB/home.html';
+
+    include_once "primeiroComando.php";
+
+    //CLASSE DE EXECUÇÃO
+    falar();
+
+
+
+
+    include_once '../../../B7WEB/footer.html';
+    
+?>
