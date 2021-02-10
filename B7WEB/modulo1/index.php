8033a803c898d73be0e6852bf7ba86dc732237a3
werikesantos <werike.ms@gmail.com>
2021-02-10 01:05:07 -0300

    Ajustado rotas

--- B7WEB/modulo1/index.php
+++ B7WEB/modulo1/index.php
@@ -14,16 +14,16 @@
             <dd><button><a href="../modulo1/aula01/main.php">Exercício - 01</a></button></dd>
             <dd><button><a href="../modulo1/aula01/main.php">Exercício - 02</a></button></dd>
             <dd><button><a href="../modulo1/aula01/main.php">Anotações</a></button></dd>
-        <dt><button><a href="../modulo1/aula01/main.php">#2 - Ferramentas necessárias</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#3 - Editor de código (VScode)</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#4 - Instalando PHP (xampp)</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#5 - Como funciona o PHP</a></button></dt>
+        <dt><button><a href="../modulo1/aula02/main.php">#2 - Ferramentas necessárias</a></button></dt>
+        <dt><button><a href="../modulo1/aula03/main.php">#3 - Editor de código (VScode)</a></button></dt>
+        <dt><button><a href="../modulo1/aula04/main.php">#4 - Instalando PHP (xampp)</a></button></dt>
+        <dt><button><a href="../modulo1/aula05/main.php">#5 - Como funciona o PHP</a></button></dt>
         <dt><button><a href="../modulo1/aula06/main.php">#6 - Primeiros comandos no PHP</a></button></dt>
         <dt><button><a href="../modulo1/aula07/main.php">#7 - Tipos de variáveis</a></button></dt>
         <dt><button><a href="../modulo1/aula08/main.php">#8 - Mesclando Variáveis</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#9 - Arrays</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#10 - Operador Arrays Spreed</a></button></dt>
-        <dt><button><a href="../modulo1/aula01/main.php">#11 - Exercício Prático</a></button></dt>
+        <dt><button><a href="../modulo1/aula09/main.php">#9 - Arrays</a></button></dt>
+        <dt><button><a href="../modulo1/aula10/main.php">#10 - Operador Arrays Spreed</a></button></dt>
+        <dt><button><a href="../modulo1/aula11/main.php">#11 - Exercício Prático</a></button></dt>
     </dl>
 
 <?php
